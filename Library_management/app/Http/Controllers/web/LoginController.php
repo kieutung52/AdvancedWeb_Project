485b4f09c4a0c0ae64bfceb89a1dec825708d340
kieutung <kieuthanhtung0502@gmail.com>
2025-01-16 12:10:42 +0700

    edit login controller

---
+++ Library_management/app/Http/Controllers/web/LoginController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'password' => 'required|string',
+            ], [
+                'name.required' => 'name is required.',
+                'name.name' => 'Invalid name format.',
+                'password.required' => 'Password is required.',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
+            if (Auth::attempt($request->only('name', 'password'))) {
+                $user = Auth::user();
+
+                if($user->role === 'admin'){
+                    return redirect()->route('dashboard');
+                }
+                return redirect()->route('userhome');
+            }
+            return redirect()->back()->withErrors(['password' => 'Incorrect name or password.'])->withInput();
+        } catch (ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->withInput();
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            Auth::login($user);
+            if ($user->role === 'admin') {
+                return redirect()->route('dashboard');
+            }
+    
+            return redirect()->route('userhome');
+        } catch (ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->withInput();
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout(); 
+        return redirect()->route('login');
+    }
+}
