7c6c1b432ab59d472571418810e848b791793010
kieutung <kieuthanhtung0502@gmail.com>
2025-01-16 12:06:40 +0700

    add fonts and config vite

--- Library_management/vite.config.js
+++ Library_management/vite.config.js
@@ -4,8 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js', 
+                'resources/css/admin/dashboard.css', 
+                'resources/css/login/login.css',
+                'resources/css/admin/display.css'
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@fortawesome': '/node_modules/@fortawesome', // Sử dụng đường dẫn tuyệt đối cho Font Awesome
+        },
+    },
 });
